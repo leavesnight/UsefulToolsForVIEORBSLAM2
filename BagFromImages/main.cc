53d7bb0f1ceed526c3eb4fa3cf07b26c2efa35dc
Zhanghao Zhu <zhuzhanghao@wywk.cn>
2021-08-27 13:19:09 +0800

    ENH: change the interface in BagFromImages
        1.for universal bag from/to images folder
        and imu file, we change the interfaces now
    
    TEST: passed on datasets like EuRoC's bag and our datasets

--- BagFromImages/main.cc
+++ BagFromImages/main.cc
@@ -1,15 +1,15 @@
-#include<iostream>
-#include<fstream>
-#include<ros/ros.h>
-#include<rosbag/bag.h>
-#include<rosbag/view.h>
-#include<sensor_msgs/Image.h>
-#include<std_msgs/Time.h>
-#include<std_msgs/Header.h>
+#include <cv_bridge/cv_bridge.h>
+#include <fstream>
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/Time.h>
 
 #include "Thirdparty/DLib/FileFunctions.h"
 
@@ -21,162 +21,255 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "BagFromImages");
+typedef enum ID_MODE {
+  MODE_FREQUENCY = 1,
+  MODE_IMU_DATA_INFO = 2,
+  //<path to imagei directory> is input when bit4 not on, output when it's on
+  MODE_IN_IMAGES_BAG = 4
+} eID_MODE;
 
-    switch(argc)
-    {
-      case 5:case 6:case 7:case 8:
-	break;
-      default:
-        cerr << "Usage: rosrun BagFromImages BagFromImages <path to image directory> <image extension .ext> <frequency> <path to output bag> (<depth extension .ext>) (number of IMU data) (color images' rosbag's name)" << endl;
-        return 0;
+typedef enum SIZE_ARG {
+  SIZE_ID_MODE = 1,
+  SIZE_FREQUENCY = 1,
+  SIZE_IMU_DATA_INFO = 6,
+  SIZE_IMAGES_BAG_PATH = 1,
+  SIZE_IMAGE_FOLDER_INFO = 4
+} eSIZE_ARG;
+
+void printusage() {
+  cerr << "Usage: [rosrun BagFromImages or ./]BagFromImages <id_mode> "
+          "[{id_mode&1:}<frequency> or {!(id_mode&1):<scale_name_time>}] "
+          "[{id_mode&4:}<path to input bag> else <path to output bag>]"
+          "({id_mode&2:}<num of IMU> <start_line> <path to imu file> <imu "
+          "topic name> <num of IMU data:13/7/6> <scale_acc> <scale_gyro> "
+          "<scale_time>) <path to imu1 file>..."
+          "(<path to image0 directory> <image0 extension e.g. bmp> <image0 "
+          "topic name> <image0 channels num> (<path to depth/image1 "
+          "directory>...)"
+       << endl;
+  cerr << "<number of IMU data> doesn't include timestamp(default 0th data):"
+          " 13 means qw qx qy qz mx my mz ax ay az gx gy gz>; 6 means ax ay az "
+          "gx gy gz; 7 means id_imu ax ay az gx gy gz{so 7 could allow to "
+          "merge all imus in one file}"
+       << endl;
+  cerr << "<start_line> means first n lines to be skipped in such file" << endl;
+  cerr << "<imagei channels num>: 3 means Color, 1 means Depth" << endl;
+  cerr << "Current version unsupported imu output to a file from input bag"
+       << endl;
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "BagFromImages");
+
+  if (argc < 6) {
+    printusage();
+    return -1;
+  }
+
+  ros::start();
+
+  int pos = 1;
+  int id_mode = atoi(argv[pos]);
+  pos += SIZE_ID_MODE;
+
+  // Frequency
+  double freq = 30;
+  double scale_tm = 1.;
+  if (id_mode & MODE_FREQUENCY) {
+    freq = atof(argv[pos]);
+  } else {
+    scale_tm = atof(argv[pos]);
+  }
+  pos += SIZE_FREQUENCY;
+
+  // Bag Path
+  string path_in_image_bag = "";
+  rosbag::Bag *pbag_out = nullptr; // Output bag
+  if (id_mode & MODE_IN_IMAGES_BAG) {
+    path_in_image_bag = argv[pos];
+  } else {
+    pbag_out = new rosbag::Bag(argv[pos], rosbag::bagmode::Write);
+  }
+  pos += SIZE_IMAGES_BAG_PATH;
+
+  // Imu Path
+  vector<string> paths_imu_file;
+  vector<string> topics_imu; // e.g. "/imu" "/imu0"
+  vector<int> nums_imu_data; // not including timestamp but including id_imu
+  vector<int> start_lines;
+  vector<vector<double>> scales_imu; // a, g, tm
+  if (id_mode & MODE_IMU_DATA_INFO) {
+    int num = atoi(argv[pos]);
+    start_lines.push_back(atoi(argv[pos + 1]));
+    pos += 2;
+
+    for (int j = 0; j < num; ++j) {
+      paths_imu_file.push_back(argv[pos]);
+      topics_imu.push_back(argv[pos + 1]);
+      nums_imu_data.push_back(atoi(argv[pos + 2]));
+      scales_imu.push_back(vector<double>());
+      for (int i = 0; i < 3; ++i)
+        scales_imu.back().push_back(atof(argv[pos + 3 + i]));
+      pos += SIZE_IMU_DATA_INFO;
     }
+  }
 
-    ros::start();
+  // Vector of paths to image
+  vector<string> foldernames_image, exts_image;
+  vector<vector<string>> filenames_images;
+  vector<int> nums_channels;
+  fstream finIMU;
+  string strtmp;
+  int num_params_left = argc - pos;
+  int i = 0;
+  vector<string> topics_image;
+  while (num_params_left > 0) {
+    foldernames_image.push_back(argv[pos]);
+    exts_image.push_back(argv[pos + 1]);
+    filenames_images.push_back(
+        DUtils::FileFunctions::Dir(argv[pos], argv[pos + 1], true));
+    cout << "Image" << i << "'s num: " << filenames_images.back().size();
+    // like "kinect2/qhd/image_color_rect" "/kinect2/qhd/image_color"
+    topics_image.push_back(argv[pos + 2]);
+    cout << " Topic: " << topics_image.back() << endl;
+    nums_channels.push_back(atoi(argv[pos + 3]));
+    pos += SIZE_IMAGE_FOLDER_INFO;
+    num_params_left -= SIZE_IMAGE_FOLDER_INFO;
+    ++i;
+  }
 
-    // Vector of paths to image
-    vector<string> filenames,depthnames;
-    ifstream finIMU;string strtmp;
-    if (argc==5){
-      filenames = DUtils::FileFunctions::Dir(argv[1], argv[2], true);
-    }else{//6
-      string strTmp(argv[1]);
-      filenames = DUtils::FileFunctions::Dir((strTmp+"/rgb").c_str(), argv[2], true);
-      depthnames = DUtils::FileFunctions::Dir((strTmp+"/depth").c_str(),argv[5],true);
-      cout << "depth Images: " << depthnames.size() << endl;
-      if (argc>6){//with odometry files
-	finIMU.open(strTmp+"/IMUSensor.txt");
-	getline(finIMU,strtmp);getline(finIMU,strtmp);getline(finIMU,strtmp);
-	
-	if (argc>7){//save images from referenced rosbag
-	  rosbag::Bag bagin;bagin.open(string(argv[7]),rosbag::bagmode::Read);
-	  vector<string> topics;
-	  topics.push_back("/kinect2/qhd/image_color_rect");
-	  topics.push_back("/kinect2/qhd/image_color");
-	  rosbag::View view(bagin,rosbag::TopicQuery(topics));
-	  int i=0;
-	  BOOST_FOREACH(rosbag::MessageInstance const m,view){
-	    sensor_msgs::ImageConstPtr simage=m.instantiate<sensor_msgs::Image>();
-	    if (simage!=NULL){
-	      const sensor_msgs::Image &img=*simage;
-	      double tm_stamp=img.header.stamp.toSec();
-	      char ch[25];//at least 10+1+6+4+1=22
-	      sprintf(ch,"%.6f.",tm_stamp);//mpTracker->mCurrentFrame.mTimeStamp);
-	      string rgbname=strTmp+"/rgb/"+ch+"bmp";
-	      
-	      cv_bridge::CvImageConstPtr cv_ptr=cv_bridge::toCvShare(simage);
-	      cv::imwrite(rgbname,cv_ptr->image);
-	      cout<<"Saved "<<++i<<" images~"<<endl;
-	    }
-	  }
-	  vector<string> topicsD;
-	  topicsD.push_back("/kinect2/qhd/image_depth_rect");
-	  topicsD.push_back("/kinect2/qhd/image_depth");
-	  rosbag::View viewD(bagin,rosbag::TopicQuery(topicsD));
-	  i=0;
-	  BOOST_FOREACH(rosbag::MessageInstance const m,viewD){
-	    sensor_msgs::ImageConstPtr simage=m.instantiate<sensor_msgs::Image>();
-	    if (simage!=NULL){
-	      const sensor_msgs::Image &img=*simage;
-	      double tm_stamp=img.header.stamp.toSec();
-	      char ch[25];//at least 10+1+6+4+1=22
-	      sprintf(ch,"%.6f.",tm_stamp);//mpTracker->mCurrentFrame.mTimeStamp);
-	      string depthname=strTmp+"/depth/"+ch+"png";
-	      
-	      cv_bridge::CvImageConstPtr cv_ptr=cv_bridge::toCvShare(simage);
-	      cv::imwrite(depthname,cv_ptr->image);
-	      cout<<"Saved "<<++i<<" depth images~"<<endl;
-	    }
-	  }
-	}
-	
+  // path to input bag
+  if (path_in_image_bag != "") { // save images from referenced rosbag
+    rosbag::Bag bagin;
+    bagin.open(path_in_image_bag, rosbag::bagmode::Read);
+    for (size_t j = 0; j < filenames_images.size(); ++j) {
+      vector<string> topics(1, topics_image[j]);
+      rosbag::View view(bagin, rosbag::TopicQuery(topics));
+      i = 0;
+      BOOST_FOREACH (rosbag::MessageInstance const m, view) {
+        sensor_msgs::ImageConstPtr simage = m.instantiate<sensor_msgs::Image>();
+        if (simage != NULL) {
+          const sensor_msgs::Image &img = *simage;
+          double tm_stamp = img.header.stamp.toSec();
+          char ch[25]; // at least 10+1+6+4+1=22
+          // mpTracker->mCurrentFrame.mTimeStamp;
+          sprintf(ch, "%.6f.", tm_stamp);
+          string imgname = foldernames_image[j] + "/" + ch + exts_image[j];
+
+          cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(simage);
+          cv::imwrite(imgname, cv_ptr->image);
+          cout << "Saved " << ++i << " images~" << endl;
+        }
       }
     }
+    cout << "Saved Num Images Type: " << filenames_images.size() << endl;
+  } else {
+    assert(pbag_out);
+    rosbag::Bag &bag_out = *pbag_out;
 
-    cout << "Images: " << filenames.size() << endl;
+    ros::Time t, t_old = ros::Time::now();
+    t = t_old;
 
-    // Frequency
-    double freq = atof(argv[3]);
-
-    // Output bag
-    rosbag::Bag bag_out(argv[4],rosbag::bagmode::Write);
-
-    ros::Time t,t_old = ros::Time::now();
-    t=t_old;
-
-    const float T=1.0f/freq;
+    const float T = 1.0f / freq;
     ros::Duration d(T);
 
-    for(size_t i=0;i<filenames.size();i++)
-    {
-        if(!ros::ok())
-            break;
+    // write depth/color images to rosbag::Bag
+    for (size_t j = 0; j < filenames_images.size(); ++j) {
+      t = t_old;
+      for (size_t i = 0; i < filenames_images[j].size(); i++) {
+        if (!ros::ok())
+          break;
 
-        cv::Mat im = cv::imread(filenames[i],CV_LOAD_IMAGE_COLOR);
+        string &filename = filenames_images[j][i];
         cv_bridge::CvImage cvImage;
+        cv::Mat im;
+        if (1 == nums_channels[j]) {
+          im = cv::imread(filename, cv::IMREAD_UNCHANGED);
+          cvImage.encoding = sensor_msgs::image_encodings::TYPE_16UC1;
+        } else {
+          im = cv::imread(filename, cv::IMREAD_COLOR);
+          cvImage.encoding = sensor_msgs::image_encodings::RGB8;
+        }
+        if (!(id_mode & MODE_FREQUENCY)) {
+          int pos = filename.rfind('/') + 1;
+          t = ros::Time(
+              scale_tm *
+              atof(filename.substr(pos, filename.rfind(exts_image[j]) - pos)
+                       .c_str()));
+        }
         cvImage.image = im;
-	if (argc==5){
-	  cvImage.encoding = sensor_msgs::image_encodings::RGB8;
-	}else{
-	  //cvImage.encoding=sensor_msgs::image_encodings::BGR8;
-	  cvImage.encoding = sensor_msgs::image_encodings::RGB8;
-	  int pos=filenames[i].rfind('/')+1;
-	  t=ros::Time(atof(filenames[i].substr(pos,filenames[i].rfind(argv[2])-pos).c_str()));
-	}
         cvImage.header.stamp = t;
-	if (argc==5){
-	  bag_out.write("/camera/image_raw",ros::Time(t),cvImage.toImageMsg());
-	}else{
-	  bag_out.write("/cam0/rgb/image",ros::Time(t),cvImage.toImageMsg());
-	}
-        t+=d;
-        cout << i+1 << " / " << filenames.size() << endl;
+        // e.g. "/camera/image_raw" "/cam0/rgb/image"
+        bag_out.write(topics_image[j], ros::Time(t), cvImage.toImageMsg());
+        t += d;
+        if (!(i % (int)freq) || i == filenames_images[j].size() - 1)
+          cout << i + 1 << " / " << filenames_images[j].size() << " ";
+      }
+      cout << endl;
     }
-    if (argc>5){//write depth images to rosbag::Bag
-      t=t_old;
-      for(size_t i=0;i<depthnames.size();i++)
-      {
-	  if(!ros::ok())
-	      break;
 
-	  cv::Mat im = cv::imread(depthnames[i],CV_LOAD_IMAGE_UNCHANGED);
-	  cv_bridge::CvImage cvImage;
-	  cvImage.image = im;
-	  cvImage.encoding = sensor_msgs::image_encodings::TYPE_16UC1;
-	  int pos=depthnames[i].rfind('/')+1;
-	  t=ros::Time(atof((depthnames[i].substr(pos,depthnames[i].rfind(argv[5])-pos)).c_str()));
-	  cvImage.header.stamp = t;
-	  bag_out.write("/cam0/depth/image",ros::Time(t),cvImage.toImageMsg());
-	  cout << i+1 << " / " << depthnames.size() << endl;
-      }
-      if (argc>6){//write IMU data to rosbag
-	double tmstamp,data[9],quat[4];//9/13
-	do{
-	  finIMU>>tmstamp;
-	  sensor_msgs::ImuPtr pdataIMU(new sensor_msgs::Imu());
-	  sensor_msgs::Imu &dataIMU=*pdataIMU;
-	  if (atoi(argv[6])==13){
-	    finIMU>>quat[3]>>quat[0]>>quat[1]>>quat[2];//wxyz while q3 is w
-	    dataIMU.orientation.w=quat[3];dataIMU.orientation.x=quat[0];dataIMU.orientation.y=quat[1];dataIMU.orientation.z=quat[2];
-	  }
-	  for (int i=0;i<9;++i) finIMU>>data[i];//magxyz axyz wxyz
-	  getline(finIMU,strtmp);
-	  dataIMU.angular_velocity.x=data[6];dataIMU.angular_velocity.y=data[7];dataIMU.angular_velocity.z=data[8];
-	  for (int i=3;i<6;++i) data[i]*=9.81;
-	  dataIMU.linear_acceleration.x=data[3];dataIMU.linear_acceleration.y=data[4];dataIMU.linear_acceleration.z=data[5];
-// 	  tmstamp+=0.06;
-	  t=ros::Time(tmstamp);
-	  dataIMU.header.stamp=t;
-	  bag_out.write("/imu0",t,pdataIMU);
-	}while (!finIMU.eof());
-      }
+    // write IMU data to rosbag
+    for (size_t j = 0; j < paths_imu_file.size(); ++j) {
+      finIMU.open(paths_imu_file[j], std::ios::in);
+      for (int i = 0; i < start_lines[0]; ++i)
+        getline(finIMU, strtmp);
+
+      double tmstamp, data[9], quat[4]; // max9+4=13
+      int id_imu = -1, pos_gx = 3, pos_ax = 0;
+      int line_id = 0;
+      do {
+        finIMU >> tmstamp;
+        if (finIMU.fail() || finIMU.eof())
+          break;
+        tmstamp *= scales_imu[j][2];
+        sensor_msgs::ImuPtr pdataIMU(new sensor_msgs::Imu());
+        sensor_msgs::Imu &dataIMU = *pdataIMU;
+        if (nums_imu_data[j] == 13) {
+          // wxyz while q3 is w
+          finIMU >> quat[3] >> quat[0] >> quat[1] >> quat[2];
+          dataIMU.orientation.w = quat[3];
+          dataIMU.orientation.x = quat[0];
+          dataIMU.orientation.y = quat[1];
+          dataIMU.orientation.z = quat[2];
+          for (int i = 0; i < 3; ++i)
+            finIMU >> data[i]; // magxyz
+          pos_gx = 6;
+          pos_ax = 3;
+        } else if (nums_imu_data[j] == 7) {
+          finIMU >> id_imu;
+        }
+        for (int i = 0; i < 6; ++i)
+          finIMU >> data[i]; // axyz wxyz
+        getline(finIMU, strtmp);
+
+        for (int i = pos_gx; i < pos_gx + 3; ++i)
+          data[i] *= scales_imu[j][1];
+        for (int i = pos_ax; i < pos_ax + 3; ++i)
+          data[i] *= scales_imu[j][0];
+        dataIMU.angular_velocity.x = data[pos_gx];
+        dataIMU.angular_velocity.y = data[pos_gx + 1];
+        dataIMU.angular_velocity.z = data[pos_gx + 2];
+        dataIMU.linear_acceleration.x = data[pos_ax];
+        dataIMU.linear_acceleration.y = data[pos_ax + 1];
+        dataIMU.linear_acceleration.z = data[pos_ax + 2];
+        if (!(line_id++))
+          cout << fixed << setprecision(9) << "tmstemp_start=" << tmstamp
+               << endl;
+        t = ros::Time(tmstamp);
+        dataIMU.header.stamp = t;
+        bag_out.write(topics_imu[j] + (id_imu >= 0 ? to_string(id_imu) : ""), t,
+                      pdataIMU);
+      } while (!finIMU.eof());
+      cout << "tmstemp_end=" << tmstamp << ", imu_size=" << line_id << endl;
     }
 
     bag_out.close();
+  }
 
-    ros::shutdown();
+  ros::shutdown();
 
-    return 0;
+  if (pbag_out)
+    delete pbag_out;
+
+  return 0;
 }
